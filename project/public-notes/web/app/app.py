b558f4e03a18c6247d95bf401ffb3815c3162fac
LuskaDuarte026 <lucas.sales@usp.br>
2025-05-13 11:56:18 +0200

    first html version

--- project/public-notes/web/app/app.py
+++ project/public-notes/web/app/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # from app import routes
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def main_page():
+    return render_template('index.html')
+
+
+
+
 if __name__ == '__main__':
     # Run the Flask app
     app.run(host='0.0.0.0', port=5000, debug=True)
