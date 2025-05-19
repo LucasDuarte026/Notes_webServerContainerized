3373ebbd4284e050d9cc3ea8cc3907d4073eb3c7
LuskaDuarte026 <lucas.sales@usp.br>
2025-05-19 14:06:41 +0200

    creating the first table

--- project/public-notes/db/init.sql
+++ project/public-notes/db/init.sql
@@ -2,12 +2,12 @@ CREATE DATABASE pn_database;
 CREATE USER lucas WITH PASSWORD '1234';
 ALTER DATABASE pn_database OWNER TO lucas;
 
--- \c pn_database
--- CREATE TABLE notes (
---     tag NUMERIC(4) PRIMARY KEY,
---     name VARCHAR(255) NOT NULL,
---     email VARCHAR(255) UNIQUE NOT NULL,
---     text TEXT NOT NULL
--- );
--- ALTER TABLE notes OWNER TO lucas;
+\c pn_database
+CREATE TABLE notes (
+    tag NUMERIC(4) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    text TEXT NOT NULL
+);
+ALTER TABLE notes OWNER TO lucas;
 
